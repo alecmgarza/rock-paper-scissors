bca1b638bd1b760071fffed1e65068286b98e86a
Alec Garza <alecmgarza@gmail.com>
2022-05-16 19:44:11 -0500

    Create a function that plays a round between the computer and player where the player always selects rock

--- scripts.js
+++ scripts.js
@@ -4,4 +4,19 @@ function computerPlay() {
     return rps[Math.floor(Math.random() * rps.length)];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+        return 'You lose! Paper beats Rock';
+    } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
+        return 'You win! Rock beats Scissors';
+    } else {
+        return 'Tie, try again';
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log('Computer selects: ' + computerSelection);
+console.log('Player selects: ' + playerSelection);
+console.log(playRound(playerSelection, computerSelection));
