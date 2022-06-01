deccf24063c7256a8efabc856e38cef26894fb37
Alec Garza <alecmgarza@gmail.com>
2022-05-31 23:16:48 -0500

    Create a scoreboard and an ending message for win/lose/tie after 5 rounds are completed

--- scripts.js
+++ scripts.js
@@ -1,54 +1,51 @@
 const rps = ['rock', 'paper', 'scissors'];
+const computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return rps[Math.floor(Math.random() * rps.length)];
 }
 
-function playerPlay() {
-
-}
-
 function playRound(playerSelection, computerSelection) {
+
     console.log('Computer selects: ' + computerSelection);
     console.log('Player selects: ' + playerSelection);
 
 
-    
-
     if ((computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissors' && playerSelection === 'paper') || 
     (computerSelection === 'rock' && playerSelection === 'scissors')) {
-        alert(`You lose, ${computerSelection} beats ${playerSelection}!`);
+        computerScore = computerScore + 1;
+        alert(`You lose, ${computerSelection} beats ${playerSelection}!\n\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
     } else if ((computerSelection === 'scissors' && playerSelection === 'rock') || (computerSelection === 'paper' && playerSelection === 'scissors') ||
     (computerSelection === 'rock' && playerSelection === 'paper')) {
-        alert(`You win, ${playerSelection} beats ${computerSelection}!`);
+        playerScore = playerScore + 1;
+        alert(`You win, ${playerSelection} beats ${computerSelection}!\n\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
     } else {
-        alert('Tie, try again.');
-    }
-}
+        alert(`Tie, try again.\n\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+    };
 
-function computerScore() { //work on this
-    i = 0;
-}
-
-function playerScore() { //work on this
-    i = 0;
-    console.log(i + 1);
 }
 
 function game() {
-    let playerInput = prompt('Type rock, paper, or scissors');
-    const playerSelection = playerInput.toLowerCase();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+
+    for (let i = 0; i < 5; i++) {
+        let playerInput = prompt('Type rock, paper, or scissors');
+        const playerSelection = playerInput.toLowerCase();
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    };
+
+    if (playerScore > computerScore) {
+        alert('Congrats, you win!');
+    } else if (computerScore > playerScore) {
+        alert('Sorry, you lose!');
+    } else {
+        alert('Tie game!');
+    };
+
 }
 
-for (let i = 0; i < 5; i++) {
-    game();
-}
+game();
 
-const computerSelection = computerPlay();
-const playerSelection = playerSelection.toLowerCase();
-
-
-//create functions to keep score
-//make playerSelection case-insensitive
+//REMINDER: scoreboard done, try to loop back to beginning on game over
